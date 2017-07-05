332f9af9b2d1086c71f18538c81f323a664a0b72
Renaud009 <renaud.hebert-legault@crim.ca>
2017-07-05 17:17:35 +0100

    Added datasources file for NODE_ENV=production

---
+++ server/datasources.production.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "db": {
+    "host": process.env.POSTGRES_HOST || "localhost",
+    "port": 5432,
+    "url": "",
+    "database": process.env.POSTGRES_DB || "pavics",
+    "password": process.env.POSTGRES_PASSWORD || "qwerty",
+    "name": "db",
+    "user": process.env.POSTGRES_USER || "pavics",
+    "connector": "postgresql"
+  }
+};
